4904b289e341ef04936fe58f143c1dcc33918973
dlsgur <dlsgur2845@naver.com>
2022-04-07 18:03:43 +0900

    .

--- 백준/그리디/폴리오미노.py
+++ 백준/그리디/폴리오미노.py
@@ -4,3 +4,23 @@ sys.stdin = open('../input.txt', 'r')
 import sys
 input = lambda:sys.stdin.readline().rstrip()
 
+board = input()
+
+answer=[]
+for b in board.split('.'):
+    n = len(b)
+    if n==0:
+        answer.append('')
+        continue
+
+    cnt4, n = divmod(n, 4)
+    cnt2, n = divmod(n, 2)
+
+    if n==0:
+        answer.append('AAAA'*cnt4 + 'BB'*cnt2)
+answer = '.'.join(answer)
+
+if len(answer) == len(board):
+    print(answer)
+else:
+    print(-1)
